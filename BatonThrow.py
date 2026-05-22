b84f0d154b396bc68895974e8fec28a8e4eac52c
Jerry Wang <jerryw28@nycstudents.net>
2026-05-22 14:50:05 +0000

    Effectively finished with basic projectile motion.

--- BatonThrow.py
+++ BatonThrow.py
@@ -8,28 +8,25 @@ g = -9.81
 m = 0.1
 initPos = vec(0, 0, 0)
 rodPos = initPos
-Ball1Pos = initPos - vec(sqrt(2)/2,sqrt(2)/2,0)
-Ball2Pos = initPos + vec(sqrt(2)/2,sqrt(2)/2,0)
+
 initVel = vec(10, 10, 0)
 rodVel = initVel
 rodAcc = vec(0, g, 0)
 len = 3;
 baton = cylinder(pos = rodPos, axis = len * norm(rodVel), color=color.red, radius = .5)
-ball1 = sphere(pos= Ball1Pos, radius = 0.5, color=color.red)
-ball2 = sphere(pos= Ball2Pos, radius = 0.5, color=color.red)
+ball1 = sphere(pos= rodPos - .1 * (len * norm(rodVel)) , radius = 0.7, color=color.red)
+ball2 = sphere(pos= rodPos + 1.1 * len * norm(rodVel), radius = 0.7, color=color.red)
 
 while rodPos.y >= 0:
     rate(1 / dt)
     
     rodVel += rodAcc * dt
     rodPos += rodVel * dt
-    Ball1Pos += rodVel * dt
-    Ball2Pos += rodVel * dt
     
     baton.pos = rodPos
     baton.axis = len * norm(rodVel)
-    ball1.pos = Ball1Pos
-    ball2.pos = Ball2Pos
+    ball1.pos = rodPos - .1 * (len * norm(rodVel))
+    ball2.pos = rodPos + 1.1 * len * norm(rodVel)
     
     t += dt
     
