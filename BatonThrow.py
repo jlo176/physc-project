e0c2423c1badecd0a0fc82342712d9510ae826ce
Jerry Wang <jerryw28@nycstudents.net>
2026-05-22 14:39:09 +0000

    Added spheres at each end.

--- BatonThrow.py
+++ BatonThrow.py
@@ -1,31 +1,35 @@
 Web VPython 3.2
 
-t = 0.00
+scene.camera.pos = vec(10, 5, 15)
+
+t = 0
 dt = 0.01
-m = 0.0088
 g = -9.81
-vy = 0
-y_init = 5
-yy = y_init
-ball = cylinder(pos=vector(0, y_init, 0), radius = 0.5, color=color.red)
-x_init = 0
-xx = x_init
-vx = 5
+m = 0.1
+initPos = vec(0, 0, 0)
+rodPos = initPos
+Ball1Pos = initPos - vec(sqrt(2)/2,sqrt(2)/2,0)
+Ball2Pos = initPos + vec(sqrt(2)/2,sqrt(2)/2,0)
+initVel = vec(10, 10, 0)
+rodVel = initVel
+rodAcc = vec(0, g, 0)
+len = 3;
+baton = cylinder(pos = rodPos, axis = len * norm(rodVel), color=color.red, radius = .5)
+ball1 = sphere(pos= Ball1Pos, radius = 0.5, color=color.red)
+ball2 = sphere(pos= Ball2Pos, radius = 0.5, color=color.red)
 
-scene.camera.pos = vector(0, y_init/2, 10)
-
-while yy > 0:
-    rate(1/dt)
-    fy = m*g 
-    ay = fy / m
-    vy = vy + ay*dt
-    yy = yy + vy*dt
-    xx = xx + vx*dt
-
-    ball.pos = vector(xx, yy, 0)
-
-    t = t + dt
-
-
+while rodPos.y >= 0:
+    rate(1 / dt)
     
+    rodVel += rodAcc * dt
+    rodPos += rodVel * dt
+    Ball1Pos += rodVel * dt
+    Ball2Pos += rodVel * dt
+    
+    baton.pos = rodPos
+    baton.axis = len * norm(rodVel)
+    ball1.pos = Ball1Pos
+    ball2.pos = Ball2Pos
+    
+    t += dt
     
