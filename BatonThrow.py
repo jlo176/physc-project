145db8b749730497c26264a6212833bb38e9589b
jlo176 <jaydenl176@nycstudents.net>
2026-05-27 09:47:35 -0400

    add launch button and com calc

--- BatonThrow.py
+++ BatonThrow.py
@@ -5,7 +5,7 @@ Web VPython 3.2
  
 scene.background = color.black
 scene.camera.pos = vec(8, 5, 20)
-scene.camera.axis = vec(-8, -3, -20)
+scene.camera.axis = vec(0, 0, -20)
 g = -9.81; dt = 0.005; t = 0
 speed = 12; angle = 50; L = 3; w = 4
 m1 = 0.3; m2 = 0.3
@@ -17,11 +17,11 @@ th = 0
 v = True
  
 ax = vec(cos(th), sin(th), 0)
-rod = cylinder(pos=pos - (L/2)*ax, axis=L*ax, radius=0.08, color=color.orange)
+rod = cylinder(pos=pos - (L/2)*ax, axis=L*ax, radius=0.08, color=color.red)
 b1 = sphere(pos=pos - (L/2)*ax, radius=0.22, color=color.white)
 b2 = sphere(pos=pos + (L/2)*ax, radius=0.22, color=color.white)
  
-com = sphere(pos=pos, radius=0.10, color=color.yellow, opacity=0.6)
+com = sphere(pos=(b1.pos * m1 + b2.pos * m2) / (m1 + m2), radius=0.10, color=color.yellow, opacity=0.6)
 b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
  
 scene.append_to_caption("speed=12 angle=50 w=4 L=3\nyellow = COM path\n")
@@ -44,10 +44,17 @@ def update_m2(s):
     m2 = s.value
     m2_text.text = " {:.2f} kg".format(s.value)
 
+launched = False
+launch_btn = button(text = "launch", pos = scene.title_anchor, bind = launch)
+
+def launch():
+    global launched
+    launched = True
+
 reset_btn = button(bind=reset_action, text="Reset Simulation", pos=scene.title_anchor)
 
 def reset_action(btn):
-    global vel, pos, th, t, v, b
+    global vel, pos, th, t, v, b, launched
     t = 0
 
     rad = angle * pi / 180
@@ -63,14 +70,17 @@ def reset_action(btn):
     b1.radius = m1 * 0.8
     b2.pos = pos + (L/2)*ax
     b2.radius = m2 * 0.8
-    com.pos = pos
+    com.pos = (b1.pos * m1 + b2.pos * m2) / (m1 + m2)
     b.stop()
     b.clear()
     b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
+    
+    launched = False
+    
 while True:
     rate(1/dt)
 
-    if not v:
+    if (not v) or (not launched):
         continue
 
     vel += vec(0, g, 0) * dt
@@ -82,9 +92,13 @@ while True:
     rod.axis = L*ax
     b1.pos = pos - (L/2)*ax
     b2.pos = pos + (L/2)*ax
-    com.pos = pos
+    com.pos = (b1.pos * m1 + b2.pos * m2) / (m1 + m2)
 
     t += dt
+
+    if pos.y <= 0:
+        v = False
+        
 
     if pos.y <= 0:
         v = False
