f105ed65b45d066111ff68fef527443ad678c384
Jerry Wang <jerryw28@nycstudents.net>
2026-05-26 22:04:30 -0400

    Added sliders for the two ball massses; motion not acccounted for.

--- BatonThrow.py
+++ BatonThrow.py
@@ -1,31 +1,48 @@
 Web VPython 3.2
  
-# rod projectile sim 
+# rod projectile sim
 # shows baton flying with COM trail, spins independently
  
 scene.background = color.black
 scene.camera.pos = vec(8, 5, 20)
 scene.camera.axis = vec(-8, -3, -20)
 g = -9.81; dt = 0.005; t = 0
-speed = 12; angle = 50; L = 3; w = 4  # w = angular velocity
+speed = 12; angle = 50; L = 3; w = 4
+m1 = 0.3; m2 = 0.3
  
 rad = angle * pi / 180
 vel = speed * vec(cos(rad), sin(rad), 0)
 pos = vec(0, 0.01, 0)
-th = 0  # rotation angle
+th = 0
 v = True
  
-# baton pieces
 ax = vec(cos(th), sin(th), 0)
 rod = cylinder(pos=pos - (L/2)*ax, axis=L*ax, radius=0.08, color=color.orange)
 b1 = sphere(pos=pos - (L/2)*ax, radius=0.22, color=color.white)
 b2 = sphere(pos=pos + (L/2)*ax, radius=0.22, color=color.white)
  
-# COM dot + trail
 com = sphere(pos=pos, radius=0.10, color=color.yellow, opacity=0.6)
 b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
  
 scene.append_to_caption("speed=12 angle=50 w=4 L=3\nyellow = COM path\n")
+
+scene.append_to_caption("\n\nBall 1 mass: ")
+m1_slider = slider(min=0.05, max=1.0, value=0.3, step=0.05, bind=update_m1)
+m1_text = wtext(text=" 0.30 kg")
+
+scene.append_to_caption("\n\nBall 2 mass: ")
+m2_slider = slider(min=0.05, max=1.0, value=0.3, step=0.05, bind=update_m2)
+m2_text = wtext(text=" 0.30 kg")
+
+def update_m1(s):
+    global m1
+    m1 = s.value
+    m1_text.text = " {:.2f} kg".format(s.value)
+
+def update_m2(s):
+    global m2
+    m2 = s.value
+    m2_text.text = " {:.2f} kg".format(s.value)
 
 reset_btn = button(bind=reset_action, text="Reset Simulation", pos=scene.title_anchor)
 
@@ -43,12 +60,13 @@ def reset_action(btn):
     rod.pos = pos - (L/2)*ax
     rod.axis = L*ax
     b1.pos = pos - (L/2)*ax
+    b1.radius = m1 * 0.8
     b2.pos = pos + (L/2)*ax
+    b2.radius = m2 * 0.8
     com.pos = pos
     b.stop()
     b.clear()
     b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
-
 while True:
     rate(1/dt)
 
