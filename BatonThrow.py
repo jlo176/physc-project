e9e0565ab64d0736d27a1d5cbd727d4792a8282a
Jerry Wang <jerryw28@nycstudents.net>
2026-05-28 09:56:14 -0400

    Added working speed slider and corresponding calculations

--- BatonThrow.py
+++ BatonThrow.py
@@ -23,6 +23,11 @@ b2 = sphere(pos=pos + (L/2)*ax, radius=0.22, color=color.white)
  
 com = sphere(pos=(b1.pos * m1 + b2.pos * m2) / (m1 + m2), radius=0.10, color=color.yellow, opacity=0.6)
 b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
+t_b1 = attach_trail(b1, color=color.yellow, radius=0.035, retain=500)
+t_b2 = attach_trail(b2, color=color.yellow, radius=0.035, retain=500)
+
+r1 = m2 * L / (m1 + m2)
+r2 = m1 * L / (m1 + m2)
  
 scene.append_to_caption("speed=12 angle=50 w=4 L=3\nyellow = COM path\n")
 
@@ -34,6 +39,10 @@ scene.append_to_caption("\n\nBall 2 mass: ")
 m2_slider = slider(min=0.05, max=1.0, value=0.3, step=0.05, bind=update_m2)
 m2_text = wtext(text=" 0.30 kg")
 
+scene.append_to_caption("\n\n Starting speed ")
+speed_slider = slider(min=1.0, max=20.0, value=10, step=1, bind=update_speed)
+speed_text = wtext(text=" 10 m/s")
+
 def update_m1(s):
     global m1
     m1 = s.value
@@ -43,18 +52,29 @@ def update_m2(s):
     global m2
     m2 = s.value
     m2_text.text = " {:.2f} kg".format(s.value)
+    
+def update_speed(s):
+    global speed
+    speed = s.value
+    speed_text.text = " {:.2f} m/s".format(s.value)
 
-launched = False
-launch_btn = button(text = "launch", pos = scene.title_anchor, bind = launch)
 
-def launch():
-    global launched
-    launched = True
+
+running = False
+run_btn = button(text = "Run", pos = scene.title_anchor, bind = run)
+
+def run():
+    global running, run_btn
+    running = not running
+    if running: 
+        run_btn.text = "Pause"
+    else:
+        run_btn.text = "Run"
 
 reset_btn = button(bind=reset_action, text="Reset Simulation", pos=scene.title_anchor)
 
 def reset_action(btn):
-    global vel, pos, th, t, v, b, launched
+    global vel, pos, th, t, v, b, t_b1, t_b2, running, run_btn
     t = 0
 
     rad = angle * pi / 180
@@ -64,23 +84,32 @@ def reset_action(btn):
     v = True
 
     ax = vec(cos(th), sin(th), 0)
-    rod.pos = pos - (L/2)*ax
-    rod.axis = L*ax
-    b1.pos = pos - (L/2)*ax
+    r1 = m2 * L / (m1 + m2)
+    r2 = m1 * L / (m1 + m2)
+    b1.pos = pos - r1 * ax
     b1.radius = m1 * 0.8
-    b2.pos = pos + (L/2)*ax
+    b2.pos = pos + r2 * ax
     b2.radius = m2 * 0.8
+    rod.pos = b1.pos
+    rod.axis = b2.pos - b1.pos
     com.pos = (b1.pos * m1 + b2.pos * m2) / (m1 + m2)
     b.stop()
     b.clear()
     b = attach_trail(com, color=color.yellow, radius=0.035, retain=500)
+    t_b1.stop()
+    t_b1.clear()
+    t_b1 = attach_trail(b1, color=color.yellow, radius=0.035, retain=500)
+    t_b2.stop()
+    t_b2.clear()
+    t_b2 = attach_trail(b2, color=color.yellow, radius=0.035, retain=500)
     
-    launched = False
+    running = False
+    run_btn.text = "Run"
     
 while True:
     rate(1/dt)
 
-    if (not v) or (not launched):
+    if (not v) or (not running):
         continue
 
     vel += vec(0, g, 0) * dt
@@ -88,10 +117,12 @@ while True:
     th += w * dt
 
     ax = vec(cos(th), sin(th), 0)
-    rod.pos = pos - (L/2)*ax
-    rod.axis = L*ax
-    b1.pos = pos - (L/2)*ax
-    b2.pos = pos + (L/2)*ax
+    r1 = m2 * L / (m1 + m2)
+    r2 = m1 * L / (m1 + m2)
+    b1.pos = pos - r1 * ax
+    b2.pos = pos + r2 * ax
+    rod.pos = b1.pos
+    rod.axis = b2.pos - b1.pos
     com.pos = (b1.pos * m1 + b2.pos * m2) / (m1 + m2)
 
     t += dt
@@ -99,6 +130,21 @@ while True:
     if pos.y <= 0:
         v = False
         
-
-    if pos.y <= 0:
-        v = False
+#def down():
+#    global drag
+#    print("its not dragging time!")
+#    if (mag(velArrow.pos + velArrow.axis - scene.mouse.pos) < slop and launched == False):
+#        drag = True
+#        print("its dragging time!")
+#        
+#def move():
+#    if drag:
+#        velArrow.axis = scene.mouse.pos
+#        rodVel = velArrow.axis
+#        print(rodVel.x)
+#        print(rodVel.y)
+#        print("its moving time!")
+#        
+#def up():
+#    global drag
+#    drag = False
